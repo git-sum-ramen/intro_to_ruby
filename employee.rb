e8cdf68190c682f5a346e9a360b7aab720ee1e36
brian <riceb53@gmail.com>
2020-10-27 20:36:13 -0400

    add Employee class

--- employee.rb
+++ employee.rb
@@ -12,15 +12,32 @@
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
 
 # hash
-employee1 = {'first_name' => "Majora", "salary" => 80000, "last_name" => "Carter",  "active" => true }
+# employee1 = {'first_name' => "Majora", "salary" => 80000, "last_name" => "Carter",  "active" => true }
 
-p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+# p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
 
-employee2 = {:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => false }
-employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: false }
+# employee2 = {:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => false }
+# employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: false }
 
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
 
 # class
 
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+  end
+end
+
+
+employee = Employee.new("Majora", "Carter", 80000, true)
+
+employee.print_info
