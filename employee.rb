d2248796f0f32e8d038e1edf4a6ade8388bdc284
brian <riceb53@gmail.com>
2020-10-27 21:53:33 -0400

    make input options hash

--- employee.rb
+++ employee.rb
@@ -28,11 +28,11 @@ class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :first_name
   
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -45,21 +45,22 @@ class Employee
 end
 
 
-employee1 = Employee.new("Majora", "Carter", 80000, true)
+# employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee1 = Employee.new({:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true})
 
 employee1.first_name = "Arojam"
 p employee1.last_name
 
 employee1.print_info
-# employee1.give_annual_raise
-# employee1.print_info
+employee1.give_annual_raise
+employee1.print_info
 
 
 
 
-employee2 = Employee.new("Danilo", "Campos", 70000, false)
+employee2 = Employee.new({:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => false})
 
-# employee2.print_info
-# employee2.give_annual_raise
-# employee2.print_info
+employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
 
